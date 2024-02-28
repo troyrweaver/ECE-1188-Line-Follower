4cfe16b500144df70fa20275687c419d0c6a85e9
unknown <troyrweaver0@gmail.com>
2024-02-28 16:10:28 -0500

    Testing .gitignore

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,107 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+C:/ti/tirslk_max_1_00_02/inc/BumpInt.c \
+C:/ti/tirslk_max_1_00_02/inc/Clock.c \
+C:/ti/tirslk_max_1_00_02/inc/CortexM.c \
+C:/ti/tirslk_max_1_00_02/inc/FlashProgram.c \
+../Lab14_EdgeInterruptsmain.c \
+C:/ti/tirslk_max_1_00_02/inc/LaunchPad.c \
+C:/ti/tirslk_max_1_00_02/inc/Motor.c \
+C:/ti/tirslk_max_1_00_02/inc/PWM.c \
+C:/ti/tirslk_max_1_00_02/inc/Reflectance.c \
+C:/ti/tirslk_max_1_00_02/inc/SysTickInts.c \
+C:/ti/tirslk_max_1_00_02/inc/TExaS.c \
+C:/ti/tirslk_max_1_00_02/inc/TimerA1.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./BumpInt.d \
+./Clock.d \
+./CortexM.d \
+./FlashProgram.d \
+./Lab14_EdgeInterruptsmain.d \
+./LaunchPad.d \
+./Motor.d \
+./PWM.d \
+./Reflectance.d \
+./SysTickInts.d \
+./TExaS.d \
+./TimerA1.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./BumpInt.obj \
+./Clock.obj \
+./CortexM.obj \
+./FlashProgram.obj \
+./Lab14_EdgeInterruptsmain.obj \
+./LaunchPad.obj \
+./Motor.obj \
+./PWM.obj \
+./Reflectance.obj \
+./SysTickInts.obj \
+./TExaS.obj \
+./TimerA1.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"BumpInt.obj" \
+"Clock.obj" \
+"CortexM.obj" \
+"FlashProgram.obj" \
+"Lab14_EdgeInterruptsmain.obj" \
+"LaunchPad.obj" \
+"Motor.obj" \
+"PWM.obj" \
+"Reflectance.obj" \
+"SysTickInts.obj" \
+"TExaS.obj" \
+"TimerA1.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"BumpInt.d" \
+"Clock.d" \
+"CortexM.d" \
+"FlashProgram.d" \
+"Lab14_EdgeInterruptsmain.d" \
+"LaunchPad.d" \
+"Motor.d" \
+"PWM.d" \
+"Reflectance.d" \
+"SysTickInts.d" \
+"TExaS.d" \
+"TimerA1.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"C:/ti/tirslk_max_1_00_02/inc/BumpInt.c" \
+"C:/ti/tirslk_max_1_00_02/inc/Clock.c" \
+"C:/ti/tirslk_max_1_00_02/inc/CortexM.c" \
+"C:/ti/tirslk_max_1_00_02/inc/FlashProgram.c" \
+"../Lab14_EdgeInterruptsmain.c" \
+"C:/ti/tirslk_max_1_00_02/inc/LaunchPad.c" \
+"C:/ti/tirslk_max_1_00_02/inc/Motor.c" \
+"C:/ti/tirslk_max_1_00_02/inc/PWM.c" \
+"C:/ti/tirslk_max_1_00_02/inc/Reflectance.c" \
+"C:/ti/tirslk_max_1_00_02/inc/SysTickInts.c" \
+"C:/ti/tirslk_max_1_00_02/inc/TExaS.c" \
+"C:/ti/tirslk_max_1_00_02/inc/TimerA1.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
